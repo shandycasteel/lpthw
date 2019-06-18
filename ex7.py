48bb07f4d318fe91f69b845fa60bf18f01da595a
Shandy Casteel <shandycasteel@gmail.com>
2019-06-18 01:27:49 -0500

    Start new exercises

---
+++ ex7.py
@@ -0,0 +1,39 @@
+# Prints out string
+print("Mary had a little lamb.")
+# Prints out .format string with variable assigned to a second string value
+print("It's fleece was white as {}.".format('snow'))
+# Prints out a string
+print("And everywhere that Mary went.")
+# Prints the string '.' 10 times.
+print("." * 10) # What'd that do?
+
+# Assigns the value "C" to the variable end1
+end1 = "C"
+# Assigns the value "h" to the variable end2
+end2 = "h"
+# Assigns the value "e" to the variable end3
+end3 = "e"
+# Assigns the value "e" to the variable end4
+end4 = "e"
+# Assigns the value "s" to the variable end5
+end5 = "s"
+# Assigns the value "e" to the variable end6
+end6 = "e"
+# Assigns the value "B" to the variable end7
+end7 = "B"
+# Assigns the value "u" to the variable end8
+end8 = "u"
+# Assigns the value "r" to the variable end9
+end9 = "r"
+# Assigns the value "g" to the variable end10
+end10 = "g"
+# Assigns the value "e" to the variable end11
+end11 = "e"
+# Assigns the value "r" to the variable end12
+end12 = "r"
+
+# Watch end = ' ' at the end. Try removing it to see what happens
+# Prints out concatenated strings, extra argument for no new line
+print(end1 + end2 + end3 + end4 + end5+ end6, end = " ")
+# Prints out concatenated strings
+print(end7 + end8 + end9 + end10 + end11 + end12)
