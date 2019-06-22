f9a4397308cb30af35cab8563a553df6120bf4c8
Shandy Casteel <shandycasteel@gmail.com>
2019-06-22 15:37:24 -0500

    Finish ex16

---
+++ ex16.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+target.seek(0)
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("Line 1: ")
+line2 = input("Line 2: ")
+line3 = input("Line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(f"""
+{line1}
+{line2}
+{line3}
+""")
+
+print("And finally, we close it.")
+target.close()
+
+target = open(filename)
+
+print(target.read())
+target.close()
